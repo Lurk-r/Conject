c16109578d41b8f9be3c16d08affab350552da55
Lurk[r] <215485219+Lurk-r@users.noreply.github.com>
2026-02-26 20:29:16 +0000

    Update config.h

--- src/headers/config.h
+++ src/headers/config.h
@@ -1,11 +1,8 @@
 #pragma once
 #define _CRT_SECURE_NO_WARNINGS
 
-constexpr wchar_t PATTERN_PROC[] = L"pixel*gun*.exe";
 constexpr wchar_t PATTERN_DLL[] = L"*.dll";
-constexpr wchar_t APP_ID[] = L"2524890";
 
-constexpr int DELAY_POST_LAUNCH = 10000;
 constexpr int DELAY_POST_INJECT = 5000;
 constexpr int DELAY_EXIT = 5000;
 constexpr int DELAY_RETRY = 3000;
@@ -13,7 +10,6 @@ constexpr int INJECT_DELAY = 500;
 constexpr int DELAY_HIDE_CONSOLE_TIMEOUT = 60000;
 constexpr int POLL_INTERVAL = 100;
 constexpr int POST_HIDE_DELAY = 500;
-constexpr int PROCESS_POLL_INTERVAL = 2000;
 
 constexpr size_t CONSOLE_BUFFER_SIZE = 256;
 constexpr size_t PROMPT_INPUT_SIZE = 64;
@@ -27,10 +23,10 @@ constexpr wchar_t CONSOLE_WINDOW_CLASS[] = L"ConsoleWindowClass";
 #define MSG_ERROR_ELEVATED   L"ERROR: Process may be elevated! Rerun injector as Admin.\n"
 #define MSG_ERROR_INJECT     L"ERROR: Inject failed (you may need to rerun injector as Admin)! Retrying\n"
 #define MSG_ERROR_CRASHED    L"ERROR: Process crashed!\n"
-#define MSG_NO_PROCESS       L"No matching process found. Launching\n"
-#define MSG_WAIT_PROCESS     L"Waiting for process\n"
+#define MSG_NO_PROCESS       L"ERROR: No matching processes found.\n"
 #define MSG_INJECTING_DLL    L"- Injecting "
 #define MSG_INJECT_NOTE      L"\n(if injector doesn't close after ~10s then feel free to ignore, go back to process & press keybind)\n\n"
 #define MSG_INJECT_IN        L"\rInject in %2d"
 #define MSG_EXIT_IN          L"\rExit in %2d"
 #define MSG_DLL_PROMPT       L"\nPick [1-%d, space > multi, * > all]: "
+#define MSG_PROCESS_PROMPT   L"\nPick process [1-%d]: "
